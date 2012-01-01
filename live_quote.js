a8df3321eb0f962da52cee2266ff08cbb734f0ee
Ashwanth <ashwanthkumar@googlemail.com>
2012-01-01 23:20:19 +0530

    Updated the response object from response.json to response.send with Content-Type header

--- live_quote.js
+++ live_quote.js
@@ -41,7 +41,7 @@ app.get('/', function(request, response) {
 			res.on('end', function() {
 				resp = resp.trim();
 				data = JSON.parse(resp);
-				response.json(data, { ['X-Created-By' : "Ashwanth Kumar <ashwanth@ashwanthkumar.in>" , 'X-Updated-On' : '01/01/2012']});
+				response.send(data, { ['X-Created-By' : "Ashwanth Kumar <ashwanth@ashwanthkumar.in>" , 'X-Updated-On' : '01/01/2012', 'Content-Type': 'application/json']});
 			});
 		});
 		req.end();
